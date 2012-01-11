88f1ed265331933793c251a2b51b2739b6a723f9
Amit Kumar <amit_kumar@external.mckinsey.com>
2012-01-11 00:49:50 -0500

    Modified passenger recipes for suse

--- cookbooks/passenger/recipes/install.rb
+++ cookbooks/passenger/recipes/install.rb
@@ -4,27 +4,11 @@
 
 gem_package "passenger/system" do
   package_name 'passenger'
-  version node[:passenger][:version]
-  not_if "test -e /usr/local/rvm"
+  not_if "test -e /usr/local/bin/rvm-gem.sh"
 end
 
 gem_package "passenger/rvm" do
   package_name 'passenger'
-  version node[:passenger][:version]
-  gem_binary "/usr/local/rvm/bin/rvm default exec gem"
-  only_if "test -e /usr/local/rvm"
+  gem_binary "/usr/local/bin/rvm-gem.sh"
+  only_if "test -e /usr/local/bin/rvm-gem.sh"
 end
-
-gem_package "bundler/system" do
-  package_name 'bundler'
-  not_if "test -e /usr/local/rvm"
-end
-
-gem_package "bundler/rvm" do
-  package_name 'bundler'
-  gem_binary "/usr/local/rvm/bin/rvm default exec gem"
-  only_if "test -e /usr/local/rvm"
-end
-
-node.default[:passenger][:root_path] = run_passenger_config '--root'
-node.default[:passenger][:module_path] = run_passenger_config('--root') + "/ext/apache2/mod_passenger.so"
