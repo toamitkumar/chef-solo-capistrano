a76249b57fe3c04ed31ed818c7323342860abafd
Amit Kumar <amit_kumar@external.mckinsey.com>
2012-01-12 09:56:50 -0500

    Modified the steps for installing ruby 1.9.3

--- bootstraps/ruby_1_9_3_p0.sh
+++ bootstraps/ruby_1_9_3_p0.sh
@@ -7,18 +7,33 @@ function bootstrap_ruby_1_9_3_p0 {
     echo "== Skipped, already installed"
   else
     cd /tmp
-    git clone https://github.com/sstephenson/ruby-build.git
-    cd ruby-build
-    ./install.sh
+    wget http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
+    tar xzvf yaml-0.1.4.tar.gz
+    pushd yaml-0.1.4
+      ./configure --prefix=/usr/local
+      make && make install
+    popd
 
-    ruby-build 1.9.3-p0 /usr/local/ruby-1.9.3-p0
+    wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.gz
+    tar xzvf ruby-1.9.3-p0.tar.gz
+    pushd ruby-1.9.3-p0
+      ./configure --prefix=/usr/local --enable-shared --disable-install-doc --with-opt-dir=/usr/local/lib
+      make && make install
+    popd
 
-    ln -s /usr/local/ruby-1.9.3-p0 /usr/local/ruby
-    ln -s /usr/local/ruby/bin/ruby /usr/local/bin/ruby
-    ln -s /usr/local/ruby/bin/ri /usr/local/bin/ri
-    ln -s /usr/local/ruby/bin/irb /usr/local/bin/irb
-    ln -s /usr/local/ruby/bin/rdoc /usr/local/bin/rdoc
-    ln -s /usr/local/ruby/bin/gem /usr/local/bin/gem
+    # cd /tmp
+    # git clone https://github.com/sstephenson/ruby-build.git
+    # cd ruby-build
+    # ./install.sh
+
+    # ruby-build 1.9.3-p0 /usr/local/ruby-1.9.3-p0
+
+    # ln -s /usr/local/ruby-1.9.3-p0 /usr/local/ruby
+    # ln -s /usr/local/ruby/bin/ruby /usr/local/bin/ruby
+    # ln -s /usr/local/ruby/bin/ri /usr/local/bin/ri
+    # ln -s /usr/local/ruby/bin/irb /usr/local/bin/irb
+    # ln -s /usr/local/ruby/bin/rdoc /usr/local/bin/rdoc
+    # ln -s /usr/local/ruby/bin/gem /usr/local/bin/gem
 
     echo "gem: --no-ri --no-rdoc --bindir /usr/local/bin" | tee /etc/gemrc
     echo "export PATH=$PATH:/usr/local/ruby/bin" | tee -a $HOME/.bashrc
